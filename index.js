43200c8e08b539afa8359e96b9e6a35c156e25f9
Tristan Jin <tjin368@gmail.com>
2023-09-11 14:58:50 -0400

    Initial HTML and CSS finished

---
+++ index.js
@@ -0,0 +1,5 @@
+const collection = document.getElementsByClassName("countryName");
+for (let i = 0; i < collection.length; i++) {
+    console.log(collection[i].innerText)
+    // console.log(collection[i].innerHTML)
+}
